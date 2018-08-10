055c07a06df4315e07d5831da50de8c9c426e580
liushaofei <liushaofei@qujiali.com>
2018-08-10 10:54:37 +0800

    scroo and dnd

--- src/resources/hotitems.js
+++ src/resources/hotitems.js
@@ -11,64 +11,64 @@ export const hotitems = [
       image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/milk/02.jpg',
       price: 3000
     },
-    // {
-    //   id: 3,
-    //   name: '维怡特奶条内蒙古特产奶制品儿童休闲零食酸奶条棒疙瘩奶酪400g',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/milk/03.jpg',
-    //   price: 700
-    // },
-    // {
-    //   id: 4,
-    //   name: '蒙古王38度蓝包蓝色故乡白酒整箱6瓶 475ml*6浓香型低度草原白酒',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/02.jpg',
-    //   price: 60000
-    // },
-    // {
-    //   id: 5,
-    //   name: '蒙古王55度原浆 3000ml坛酒 浓香型高度白酒 地道蒙古族口味',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/03.jpg',
-    //   price: 65000
-    // },
-    // {
-    //   id: 6,
-    //   name: '科尔沁手撕风干牛肉干400g原味内蒙特产休闲零食包邮女生好吃的',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/01.jpg',
-    //   price: 6000
-    // },
-    // {
-    //   id: 7,
-    //   name: '牛肉干❤️内蒙古牛肉干❤️超干风干牛肉干 草原达尔沁 零食特产',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/02.jpg',
-    //   price: 7000
-    // },
-    // {
-    //   id: 8,
-    //   name: '牛肉干 内蒙古风干特产肉类零食小吃 蒙亮原味香辣手撕牛肉干500g',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/03.jpg',
-    //   price: 8000
-    // },
-    // {
-    //   id: 9,
-    //   name: '蒙古舞蹈服装少数民族女蒙古舞蹈演出服舞台装蒙古剧鸿雁演出服装',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/01.jpg',
-    //   price: 20000
-    // },
-    // {
-    //   id: 10,
-    //   name: '新款藏族舞蹈服成人女藏袍舞台表演服饰蒙古西藏少数民族演出服装',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/02.jpg',
-    //   price: 30000
-    // },
-    // {
-    //   id: 11,
-    //   name: '蒙古舞蹈服装成人女蒙族服饰天堂舞曲演出服装少数民族舞台装定制',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/03.jpg',
-    //   price: 35000
-    // },
-    // {
-    //   id: 12,
-    //   name: '蒙古王44调度绿桶整箱装 550ml*6  整箱6瓶装浓香型白酒',
-    //   image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/01.jpg',
-    //   price: 50000
-    // }
+    {
+      id: 3,
+      name: '维怡特奶条内蒙古特产奶制品儿童休闲零食酸奶条棒疙瘩奶酪400g',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/milk/03.jpg',
+      price: 700
+    },
+    {
+      id: 4,
+      name: '蒙古王38度蓝包蓝色故乡白酒整箱6瓶 475ml*6浓香型低度草原白酒',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/02.jpg',
+      price: 60000
+    },
+   {
+      id: 5,
+      name: '蒙古王55度原浆 3000ml坛酒 浓香型高度白酒 地道蒙古族口味',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/03.jpg',
+      price: 65000
+    },
+    {
+      id: 6,
+      name: '科尔沁手撕风干牛肉干400g原味内蒙特产休闲零食包邮女生好吃的',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/01.jpg',
+      price: 6000
+    },
+    {
+      id: 7,
+      name: '牛肉干❤️内蒙古牛肉干❤️超干风干牛肉干 草原达尔沁 零食特产',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/02.jpg',
+      price: 7000
+    },
+    {
+      id: 8,
+      name: '牛肉干 内蒙古风干特产肉类零食小吃 蒙亮原味香辣手撕牛肉干500g',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/meat/03.jpg',
+      price: 8000
+    },
+    {
+      id: 9,
+      name: '蒙古舞蹈服装少数民族女蒙古舞蹈演出服舞台装蒙古剧鸿雁演出服装',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/01.jpg',
+      price: 20000
+    },
+    {
+      id: 10,
+      name: '新款藏族舞蹈服成人女藏袍舞台表演服饰蒙古西藏少数民族演出服装',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/02.jpg',
+      price: 30000
+    },
+    {
+      id: 11,
+      name: '蒙古舞蹈服装成人女蒙族服饰天堂舞曲演出服装少数民族舞台装定制',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/suit/03.jpg',
+      price: 35000
+    },
+    {
+      id: 12,
+      name: '蒙古王44调度绿桶整箱装 550ml*6  整箱6瓶装浓香型白酒',
+      image: 'https://kuboot-1256353119.cos.ap-beijing.myqcloud.com/image/drink/01.jpg',
+      price: 50000
+    }
   ];
